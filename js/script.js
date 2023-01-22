71f452d59b860b387f0d01a947462b452e8038ad
deli-off <dzamsedovnazif1@gmail.com>
2023-01-22 23:11:22 +0500

    finished

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let container = document.createElement('div')
+let header = document.createElement('header')
+let header__div = document.createElement('div')
+let haeder__top__left = document.createElement('div')
+let haeder__top__left__nav = document.createElement('nav')
+let haeder__top__ul = document.createElement('ul')
+let liOne = document.createElement('li')
+let liTwo = document.createElement('li')
+let liThree = document.createElement('li')
+let aOne = document.createElement('a')
+let aTwo = document.createElement('a')
+let aThree = document.createElement('a')
+let liFour = document.createElement('li')
+let liFive = document.createElement('li')
+let liSix = document.createElement('li')
+let liSeven = document.createElement('li')
+
+
+
+
